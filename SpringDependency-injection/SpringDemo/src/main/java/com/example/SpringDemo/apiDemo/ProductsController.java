da082f4a8ed7b001ca3ec45fab582276bac52ba5
= <eneskamis@yandex.com>
2021-01-03 21:22:36 +0300

    SpringBoot - JPA - Maven
    SpringBoot - JPA - Maven

---
+++ SpringDependency-injection/SpringDemo/src/main/java/com/example/SpringDemo/apiDemo/ProductsController.java
@@ -0,0 +1,22 @@
+package com.example.SpringDemo.apiDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductsController {
+	
+
+	@GetMapping("/")
+	public String get()
+	{
+		return "Laptop";
+	}
+	
+	@GetMapping("/products")
+	public String get2()
+	{
+		return "Laptop and Phone";
+	}
+
+}
